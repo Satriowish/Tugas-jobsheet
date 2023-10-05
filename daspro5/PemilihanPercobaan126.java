1f6829dcadd3a6a3e6ae8dadeae63051a7007d5d
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-05 13:44:45 +0700

    percobaan jobsheet 5

---
+++ daspro5/PemilihanPercobaan126.java
@@ -0,0 +1,20 @@
+//pemilihan percobaan 1
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan126 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+
+        System.out.println("Masukan angka");
+        int angka = input26.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka" + angka + "Bilangan Genap");
+        } else {
+        }
+
+        System.out.println("Angka " + angka + "Bilangan Ganjil");
+
+    }
+}
