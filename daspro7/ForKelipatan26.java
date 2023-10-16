10936db52911170370c82f39314033ec7ef923a9
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-16 15:06:44 +0700

    Modifikasi dengan menambahkan rata-rata

--- daspro7/ForKelipatan26.java
+++ daspro7/ForKelipatan26.java
@@ -4,6 +4,7 @@ public class ForKelipatan26 {
     public static void main(String[] args) {
         Scanner input26 = new Scanner(System.in);
         int kelipatan, total, counter;
+        double rataRata;
 
         total = 0;
         counter = 0;
@@ -14,9 +15,11 @@ public class ForKelipatan26 {
                 total += i;
                 counter++;
             }
-
         }
+        rataRata = (double) total / counter;
+
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
     }
 }
