5b32ccd9edb39488abbfb8c73ce3aae254a81624
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-16 16:52:10 +0700

    modif pertama

--- daspro7/DoWhileCuti26.java
+++ daspro7/DoWhileCuti26.java
@@ -8,28 +8,20 @@ public class DoWhileCuti26 {
         System.out.println("Masukkan jatah cuti: ");
         jatahCuti = input26.nextInt();
         do {
-            System.out.println("Apakah inging mengambil cuti (y/n)");
+            System.out.println("Apakah ingin mengambil cuti (y/n)");
             konfirmasi = input26.next();
             if (konfirmasi.equalsIgnoreCase("y")) {
-                System.out.println("Mauskkan jumlah hari cuti anda: ");
+                System.out.println("Masukkan jumlah hari cuti anda: ");
                 jumlahHari = input26.nextInt();
                 if (jumlahHari <= jatahCuti) {
                     jatahCuti -= jumlahHari;
-                    System.out.println("Sisa jatah cuti anda :" + jatahCuti);
+                    System.out.println("Sisa jatah cuti anda: " + jatahCuti);
                 } else {
                     System.out.println("Jatah cuti kurang");
-                    System.out.println("Apakah ingin mencoba lagi (y/n)");
-                    konfirmasi = input26.next();
-                    if (!konfirmasi.equalsIgnoreCase("y")) {
-                        break;
-                    }
-
                 }
             } else if (konfirmasi.equalsIgnoreCase("n")) {
                 break;
-            } else {
-
             }
-        } while (jatahCuti > 0);
+        } while (true);
     }
 }
